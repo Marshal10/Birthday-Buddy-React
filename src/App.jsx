47beb7165c424d0efffacbf1ccc704fb3dc73f1d
marshal10 <marshalmaleane123@gmail.com>
2026-01-21 23:26:35 +0530

    Clear the list

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import List from "./List";
 
 const App = () => {
   const [people, setPeople] = useState(data);
+
   return (
     <main>
       <section className="container">
         <h3>{people.length} birthdays today</h3>
         <List people={people}/>
+        <button type="button" className="btn btn-block" onClick={()=>setPeople([])}>Clear All</button>
       </section>
     </main>
   );
